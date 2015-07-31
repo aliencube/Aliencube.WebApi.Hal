fdc06fcbd0f472927cdac7c4d77e480fdb9505c8
Justin Yoo <justin.yoo@aliencube.com>
2015-08-01 00:23:49 +1000

    Update LinkedResource to inherit Link

--- src/Aliencube.WebApi.Hal/Resources/LinkedResource.cs
+++ src/Aliencube.WebApi.Hal/Resources/LinkedResource.cs
@@ -5,11 +5,19 @@ namespace Aliencube.WebApi.Hal.Resources
     /// <summary>
     /// This represents the entity for resources containing links. This must be inherited.
     /// </summary>
-    public abstract class LinkedResource
+    public abstract class LinkedResource : Link
     {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="LinkedResource" /> class.
+        /// </summary>
+        protected LinkedResource()
+        {
+            this.Links = new List<Link>();
+        }
+
         /// <summary>
         /// Gets or sets the list of <see cref="Link" /> objects.
         /// </summary>
-        public List<Link> Links { get; set; }
+        public List<Link> Links { get; private set; }
     }
 }
