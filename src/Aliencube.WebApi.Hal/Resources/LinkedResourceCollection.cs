e5948819b7a5094a14c8019be1110f8f8402f9c5
Justin Yoo <justin.yoo@aliencube.com>
2015-08-02 00:21:02 +1000

    Add LinkedResourceCollection for list of items

---
+++ src/Aliencube.WebApi.Hal/Resources/LinkedResourceCollection.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Aliencube.WebApi.Hal.Resources
+{
+    public class LinkedResourceCollection<T> : LinkedResource, ICollection<T> where T : LinkedResource
+    {
+        private readonly List<T> _items;
+
+        public LinkedResourceCollection()
+        {
+            this._items = new List<T>();
+        }
+
+        public int Count
+        {
+            get { return this._items.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this._items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this._items.GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            this._items.Add(item);
+        }
+
+        public void Clear()
+        {
+            this._items.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            var result = this._items.Contains(item);
+            return result;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            this._items.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            var result = this._items.Remove(item);
+            return result;
+        }
+    }
+}
