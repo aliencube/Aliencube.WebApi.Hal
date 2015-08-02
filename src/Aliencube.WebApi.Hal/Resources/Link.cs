01b699fbc7c1ac21c44694593f350903b5745560
Justin Yoo <justin.yoo@aliencube.com>
2015-08-02 21:10:05 +1000

    Update Link class for JSON serialisation

--- src/Aliencube.WebApi.Hal/Resources/Link.cs
+++ src/Aliencube.WebApi.Hal/Resources/Link.cs
@@ -1,4 +1,6 @@
-﻿namespace Aliencube.WebApi.Hal.Resources
+﻿using Newtonsoft.Json;
+
+namespace Aliencube.WebApi.Hal.Resources
 {
     /// <summary>
     /// This represents the entity for link.
@@ -8,11 +10,13 @@
         /// <summary>
         /// Gets or sets the rel of the link.
         /// </summary>
+        [JsonIgnore]
         public string Rel { get; set; }
 
         /// <summary>
         /// Gets or sets the href of the link.
         /// </summary>
+        [JsonProperty(PropertyName = "href")]
         public string Href { get; set; }
     }
 }
