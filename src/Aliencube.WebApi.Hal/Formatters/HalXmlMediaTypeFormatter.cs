a931ac747079a8ad0b87c9ed7d70933da3c8d6ed
Justin Yoo <justin.yoo@aliencube.com>
2015-08-05 00:22:34 +1000

    Add HalXmlMediaTypeFormatter class.

---
+++ src/Aliencube.WebApi.Hal/Formatters/HalXmlMediaTypeFormatter.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using Aliencube.WebApi.Hal.Helpers;
+using Aliencube.WebApi.Hal.Resources;
+
+namespace Aliencube.WebApi.Hal.Formatters
+{
+    /// <summary>
+    /// This repressents the formatter entity to support XML response format with HAL.
+    /// </summary>
+    public class HalXmlMediaTypeFormatter : BufferedMediaTypeFormatter
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="HalXmlMediaTypeFormatter" /> class.
+        /// </summary>
+        public HalXmlMediaTypeFormatter()
+        {
+            this.SetSupportedMediaTypes();
+        }
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="HalXmlMediaTypeFormatter" /> class.
+        /// </summary>
+        /// <param name="namespace">Default namespace.</param>
+        public HalXmlMediaTypeFormatter(string @namespace)
+            : this()
+        {
+            this.Namespace = @namespace;
+        }
+
+        /// <summary>
+        /// Gets or sets the default namespace.
+        /// </summary>
+        public string Namespace { get; set; }
+
+        /// <summary>
+        /// Determines whether the <see cref="HalXmlMediaTypeFormatter" /> can read objects of the specified type.
+        /// </summary>
+        /// <param name="type">The type of object that will be read.</param>
+        /// <returns>
+        /// Returns <c>True</c>, if objects of this type can be read, otherwise returns <c>False</c>.
+        /// </returns>
+        public override bool CanReadType(Type type)
+        {
+            var isSupportedType = FormatterHelper.IsSupportedType(type);
+            return isSupportedType;
+        }
+
+        /// <summary>
+        /// Determines whether the <see cref="HalXmlMediaTypeFormatter" /> can write objects of the specified type.
+        /// </summary>
+        /// <param name="type">The type of object that will be written.</param>
+        /// <returns>
+        /// Returns <c>True</c>, if objects of this type can be written, otherwise returns <c>False</c>.
+        /// </returns>
+        public override bool CanWriteType(Type type)
+        {
+            var isSupportedType = FormatterHelper.IsSupportedType(type);
+            return isSupportedType;
+        }
+
+        /// <summary>
+        /// Called during serialization to write an object of the specified type to the specified stream.
+        /// </summary>
+        /// <param name="type">The type of the object to write.</param>
+        /// <param name="value">The object to write.</param>
+        /// <param name="writeStream">The stream to write to.</param>
+        /// <param name="content">The <see cref="HttpContent" /> instance, if available. This can be null.</param>
+        public override void WriteToStream(Type type, object value, Stream writeStream, HttpContent content)
+        {
+            var resource = value as LinkedResource;
+            if (resource == null)
+            {
+                return;
+            }
+
+            var settings = new XmlWriterSettings()
+                           {
+                               Indent = true,
+                               OmitXmlDeclaration = false,
+                               Encoding = Encoding.UTF8
+                           };
+
+            using (var writer = XmlWriter.Create(writeStream, settings))
+            {
+                if (string.IsNullOrWhiteSpace(this.Namespace))
+                {
+                    writer.WriteStartElement("Resource");
+                }
+                else
+                {
+                    writer.WriteStartElement("Resource", this.Namespace);
+                }
+
+                SerialiseAttributes(writer, resource);
+                SerialiseResources(writer, type, resource);
+
+                writer.WriteEndElement();
+                writer.Flush();
+            }
+        }
+
+        private void SetSupportedMediaTypes()
+        {
+            this.SupportedMediaTypes.Clear();
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/xml"));
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/xml"));
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/hal+xml"));
+        }
+
+        private static void SerialiseAttributes(XmlWriter writer, LinkedResource resource)
+        {
+            if (!string.IsNullOrWhiteSpace(resource.Rel))
+            {
+                writer.WriteAttributeString("rel", resource.Rel);
+            }
+
+            if (!string.IsNullOrWhiteSpace(resource.Href))
+            {
+                writer.WriteAttributeString("href", resource.Href);
+            }
+        }
+
+        private static void SerialiseResources(XmlWriter writer, Type type, LinkedResource resource)
+        {
+            if (FormatterHelper.IsLinkedResourceCollectionType(type))
+            {
+                foreach (LinkedResource innerResource in (IEnumerable)resource)
+                {
+                    SerialiseInnerResource(writer, innerResource);
+                }
+            }
+            else
+            {
+                SerialiseInnerResource(writer, resource);
+            }
+        }
+
+        private static void SerialiseInnerResource(XmlWriter writer, LinkedResource resource)
+        {
+            writer.WriteStartElement("Resource");
+
+            SerialiseAttributes(writer, resource);
+            SerialiseLinks(writer, resource.Links);
+
+            var properties = resource.GetType()
+                                     .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                     .Where(p => !p.Name.Equals("rel", StringComparison.InvariantCultureIgnoreCase) &&
+                                                 !p.Name.Equals("href", StringComparison.InvariantCultureIgnoreCase) &&
+                                                 !p.Name.Equals("links", StringComparison.InvariantCultureIgnoreCase));
+
+            SerialiseProperties(writer, resource, properties);
+
+            writer.WriteEndElement();
+        }
+
+        private static void SerialiseLinks(XmlWriter writer, IEnumerable<Link> links)
+        {
+            foreach (var link in links)
+            {
+                writer.WriteStartElement("Link");
+                writer.WriteAttributeString("rel", link.Rel);
+                writer.WriteAttributeString("href", link.Href);
+                writer.WriteEndElement();
+            }
+        }
+
+        private static void SerialiseProperties(XmlWriter writer, LinkedResource resource, IEnumerable<PropertyInfo> properties)
+        {
+            foreach (var property in properties)
+            {
+                var propertyValue = property.GetValue(resource);
+                if (property.PropertyType.IsPrimitive || property.PropertyType == typeof(string))
+                {
+                    writer.WriteElementString(property.Name, propertyValue.ToString());
+                }
+                else
+                {
+                    var value = propertyValue as LinkedResource;
+                    if (value != null)
+                    {
+                        SerialiseInnerResource(writer, value);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Aliencube.WebApi.Hal/Helpers/FormatterHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Aliencube.WebApi.Hal.Resources;
+
+namespace Aliencube.WebApi.Hal.Helpers
+{
+    /// <summary>
+    /// This represents the helper entity for formatters.
+    /// </summary>
+    public static class FormatterHelper
+    {
+        /// <summary>
+        /// Checks whether the given type is either <see cref="LinkedResource" /> or <see cref="LinkedResourceCollection{T}" />.
+        /// </summary>
+        /// <param name="type">Type to check.</param>
+        /// <returns>Returns <c>True</c>, if the given type is either <see cref="LinkedResource" /> or <see cref="LinkedResourceCollection{T}" />; otherwise returns <c>False</c>.</returns>
+        public static bool IsSupportedType(Type type)
+        {
+            var isLinkedResourceType = IsLinkedResourceType(type);
+            var isLinkedResourceCollectionType = IsLinkedResourceCollectionType(type);
+
+            return isLinkedResourceType || isLinkedResourceCollectionType;
+        }
+
+        /// <summary>
+        /// Checks whether the given type is <see cref="LinkedResource" />.
+        /// </summary>
+        /// <param name="type">Type to check.</param>
+        /// <returns>Returns <c>True</c>, if the given type is either <see cref="LinkedResource" />; otherwise returns <c>False</c>.</returns>
+        public static bool IsLinkedResourceType(Type type)
+        {
+            return type.IsSubclassOf(typeof(LinkedResource));
+        }
+
+        /// <summary>
+        /// Checks whether the given type is <see cref="LinkedResourceCollection{T}" />.
+        /// </summary>
+        /// <param name="type">Type to check.</param>
+        /// <returns>Returns <c>True</c>, if the given type is <see cref="LinkedResourceCollection{T}" />; otherwise returns <c>False</c>.</returns>
+        public static bool IsLinkedResourceCollectionType(Type type)
+        {
+            var typeToCheck = type;
+            while (typeToCheck != null && typeToCheck != typeof(object))
+            {
+                var currentType = typeToCheck.IsGenericType ? typeToCheck.GetGenericTypeDefinition() : typeToCheck;
+                if (currentType == typeof(LinkedResourceCollection<>))
+                {
+                    return true;
+                }
+
+                typeToCheck = typeToCheck.BaseType;
+            }
+
+            return false;
+        }
+    }
+}
