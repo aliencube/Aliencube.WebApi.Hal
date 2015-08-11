6d619466209726ee9cf8305f99fc949fed3d0796
Justin Yoo <justin.yoo@aliencube.com>
2015-08-11 23:34:13 +1000

    Update test methods

--- src/Aliencube.WebApi.Hal.Tests/HalXmlMediaTypeFormatterTest.cs
+++ src/Aliencube.WebApi.Hal.Tests/HalXmlMediaTypeFormatterTest.cs
@@ -2,11 +2,14 @@
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
+
 using Aliencube.WebApi.Hal.Extensions;
 using Aliencube.WebApi.Hal.Formatters;
 using Aliencube.WebApi.Hal.Tests.Helpers;
 using Aliencube.WebApi.Hal.Tests.Models;
+
 using FluentAssertions;
+
 using NUnit.Framework;
 
 namespace Aliencube.WebApi.Hal.Tests
@@ -72,17 +75,11 @@ namespace Aliencube.WebApi.Hal.Tests
 
             var root = xml.Root;
             root.Should().NotBeNull();
+            root.HasElements.Should().BeTrue();
 
-            root.Attribute("rel").Value.Should().Be(product.Rel);
-            root.Attribute("href").Value.Should().Be(product.Href);
-
-            var attrs = root.Elements()
-                            .Where(p => !p.Name.LocalName.Equals("resource", StringComparison.InvariantCultureIgnoreCase))
-                            .Select(p => new { rel = p.Attribute("rel").Value, href = p.Attribute("href").Value });
-            var links = product.Links
-                               .Where(p => !p.Rel.Equals("self", StringComparison.InvariantCultureIgnoreCase))
-                               .Select(p => new { rel = p.Rel.ToCamelCase(), href = p.Href });
-            links.Should().BeEquivalentTo(attrs);
+            var links = root.Elements().SingleOrDefault(p => p.Name.LocalName.Equals("links", StringComparison.InvariantCultureIgnoreCase));
+            links.Should().NotBeNull();
+            links.Elements().Count().Should().BeGreaterOrEqualTo(1);
 
             var resource = root.Elements()
                                .SingleOrDefault(p => p.Name.LocalName.Equals("resource", StringComparison.InvariantCultureIgnoreCase));
@@ -96,36 +93,6 @@ namespace Aliencube.WebApi.Hal.Tests
                                       .SingleOrDefault(p => p.Name.LocalName.Equals(name, StringComparison.InvariantCultureIgnoreCase));
                 element.Should().NotBeNull();
             }
-        }
-
-        [Test]
-        public void GivenProductsShouldReturnHalXmlObject()
-        {
-            var products = ProductHelper.GetProducts(2);
-
-            XDocument xml;
-            using (var stream = new MemoryStream())
-            {
-                this._formatter.WriteToStream(typeof(Products), products, stream, null);
-                xml = FormatterHelper.ParseXmlStream(stream);
-            }
-
-            var root = xml.Root;
-            root.Should().NotBeNull();
-
-            root.Attribute("rel").Value.Should().Be(products.Rel);
-            root.Attribute("href").Value.Should().Be(products.Href);
-
-            var attrs = root.Elements()
-                            .Where(p => !p.Name.LocalName.Equals("resource", StringComparison.InvariantCultureIgnoreCase))
-                            .Select(p => new { rel = p.Attribute("rel").Value, href = p.Attribute("href").Value });
-            var links = products.Links
-                               .Where(p => !p.Rel.Equals("self", StringComparison.InvariantCultureIgnoreCase))
-                               .Select(p => new { rel = p.Rel.ToCamelCase(), href = p.Href });
-            links.Should().BeEquivalentTo(attrs);
-
-            var resources = root.Elements().Where(p => p.Name.LocalName.Equals("resource", StringComparison.InvariantCultureIgnoreCase));
-            resources.Count().Should().Be(products.Count);
         }
     }
 }
